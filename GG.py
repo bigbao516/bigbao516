1bff0af4a866567acd67b4382d8068c89af9cf8c
bigbao516 <75419832+bigbao516@users.noreply.github.com>
2020-12-09 10:01:08 +0800

    Update GG.py
    def sum(f,end):
        total=0
        for k in range(f,end+1):
            if(k%2==0):
                total+=k
        return total
    def main():
        f,end=eval(input('Enter from number,and end number:'))
        tot=sum(f,end)
        print('Sum of even numbers from %d to %d is %d'%(f,end,tot))
    main()

--- GG.py
+++ GG.py
@@ -1,6 +1,11 @@
-oddTotal=0
-k=1
-while k<=100:
-    oddTotal=oddTotal+k
-    k=k+2
-print("1+3+5+...+99=",oddTotal)
+def sum(f,end):
+    total=0
+    for k in range(f,end+1):
+        if(k%2==0):
+            total+=k
+    return total
+def main():
+    f,end=eval(input('Enter from number,and end number:'))
+    tot=sum(f,end)
+    print('Sum of even numbers from %d to %d is %d'%(f,end,tot))
+main()
